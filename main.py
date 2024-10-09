a7dc26b8d4505872b7ce51bde55e23a681161512
Daniel Tang <danieltang@daniels-air.myfiosgateway.com>
2024-10-09 17:12:06 -0400

    changes in main.py, updated bot to respond to user

--- main.py
+++ main.py
@@ -3,13 +3,24 @@ from openai import AzureOpenAI
 
 endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
 key = os.getenv("AZURE_OPENAI_KEY")
-model_name = "gpt-4"
+model_name = "gpt-4o"
 
-client = AzureOpenAI(azure_endpoint=endpoint,api_version="2024-02-01",api_key=key)
-completion = client.chat.completions.create(
-model=model_name,
-messages=[{"role": "user",
-    "content": "Who is donald trump?",  # Your question can go here
-  },], 
-)
-print(completion.choices[0].message.content)
+
+while True:
+  user_input = input("")
+
+  #converts to lowercase, and leaves
+  if user_input.lower() in ["exit", "quit"]:  # Allow user to exit the loop
+    print("Exiting the chatbot.")
+    break
+
+
+  client = AzureOpenAI(azure_endpoint=endpoint,api_version="2024-02-01",api_key=key)
+  completion = client.chat.completions.create(
+  model=model_name,
+  messages=[{"role": "user",
+      "content": user_input,  # Your question can go here
+    },], 
+  )
+
+  print(completion.choices[0].message.content)
